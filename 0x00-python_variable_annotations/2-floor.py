326fc60dc7658e43d13a59e930524593815184a0
root <root@Yusuf.localdomain>
2023-08-05 13:32:30 +0100

    added

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module contains a type-annotated function floor """
+
+
+import math
+
+def floor(n: float) -> int:
+""" This module contains a type-annotated function floor """
+
+    return math.floor(n)
