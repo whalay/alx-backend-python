58263515244a292a1fc30b06b5ef2cd110fe285d
root <root@Yusuf.localdomain>
2023-08-05 13:26:42 +0100

    added

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" This module contains a type-annotated function add """
 
 def add(a: float, b: float) -> float:
+    """ adds two numbers and return the result """
+
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" This module contains a type-annotated function concat """
 
 def concat(str1: str, str2: str) -> str:
+    """ concatenates two strings and returns the result """
+
     return str1 + str2
